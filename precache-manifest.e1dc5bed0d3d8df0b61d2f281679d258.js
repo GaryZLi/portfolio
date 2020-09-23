b1433f9dc680013b09c085acb910c8d3559fee92
garyzli <garyli94601@gmail.com>
2020-09-22 22:19:02 -0700

    Updates

---
+++ precache-manifest.e1dc5bed0d3d8df0b61d2f281679d258.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20f8621441b50a96e1e6c10ec19bf27",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ce7ea8e07c9344958eb5",
+    "url": "/portfolio/static/css/main.ed9409a3.chunk.css"
+  },
+  {
+    "revision": "61b22704a356f3272481",
+    "url": "/portfolio/static/js/2.959acb33.chunk.js"
+  },
+  {
+    "revision": "90f8df42b25649df7004ebf7bb2be3cf",
+    "url": "/portfolio/static/js/2.959acb33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7ea8e07c9344958eb5",
+    "url": "/portfolio/static/js/main.317a4d69.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "480ae3a382945ef12e0240a64f3709bf",
+    "url": "/portfolio/static/media/AiMessage1.480ae3a3.png"
+  },
+  {
+    "revision": "a56ddb86e744a586280fcfa2c606100c",
+    "url": "/portfolio/static/media/AiMessage2.a56ddb86.png"
+  },
+  {
+    "revision": "8f42e018a9340b6e4e20161b06525308",
+    "url": "/portfolio/static/media/C++.8f42e018.png"
+  },
+  {
+    "revision": "fa0b8d98820453d0e8c94deb304629cd",
+    "url": "/portfolio/static/media/CoA.fa0b8d98.png"
+  },
+  {
+    "revision": "d51634871c9397d7d7caef4e8ce80cd4",
+    "url": "/portfolio/static/media/Download1.d5163487.jpg"
+  },
+  {
+    "revision": "af914821abedda386bf3355b91d1ed1f",
+    "url": "/portfolio/static/media/Download2.af914821.png"
+  },
+  {
+    "revision": "c0f2999c4e974f0fb0b47da4f8a0113e",
+    "url": "/portfolio/static/media/Expo.c0f2999c.png"
+  },
+  {
+    "revision": "adbdc925137661fc9b8a34dcc508a251",
+    "url": "/portfolio/static/media/Flutter.adbdc925.png"
+  },
+  {
+    "revision": "bbb1818d855585be081e26090010f035",
+    "url": "/portfolio/static/media/Gary Li's Resume.bbb1818d.pdf"
+  },
+  {
+    "revision": "0e29ee2943b541c2beb4b3282160353d",
+    "url": "/portfolio/static/media/IE.0e29ee29.png"
+  },
+  {
+    "revision": "6fecb5959ac1adda9002a267c91b4ca4",
+    "url": "/portfolio/static/media/SQLite.6fecb595.png"
+  },
+  {
+    "revision": "5cc4669723ac3a18637603afb4918d42",
+    "url": "/portfolio/static/media/TranscendLife1.5cc46697.jpg"
+  },
+  {
+    "revision": "9c1382093978f5ba285c3af364f6a445",
+    "url": "/portfolio/static/media/TranscendLife3.9c138209.jpg"
+  },
+  {
+    "revision": "0ec016fa5fab015027288613e179cd21",
+    "url": "/portfolio/static/media/ULD.0ec016fa.png"
+  },
+  {
+    "revision": "ad67ce91d775e30fcfeb7f1bd83e569a",
+    "url": "/portfolio/static/media/WebsiteBlocker1.ad67ce91.png"
+  },
+  {
+    "revision": "a557c77efb332500f75783659008e697",
+    "url": "/portfolio/static/media/WebsiteBlocker2.a557c77e.png"
+  },
+  {
+    "revision": "2dedc931550d9fcee73781e14602d181",
+    "url": "/portfolio/static/media/Yelp.2dedc931.png"
+  },
+  {
+    "revision": "ca6eef807b688289f480f691f1416ae6",
+    "url": "/portfolio/static/media/cloud.ca6eef80.png"
+  },
+  {
+    "revision": "48e2daf2b08490ee6805af500996d2a0",
+    "url": "/portfolio/static/media/cloud1.48e2daf2.png"
+  },
+  {
+    "revision": "30331244381562e582a48550999390b3",
+    "url": "/portfolio/static/media/cloud2.30331244.png"
+  },
+  {
+    "revision": "7bca23a516769d7a4fed7a3aa18423e4",
+    "url": "/portfolio/static/media/cloud3.7bca23a5.png"
+  },
+  {
+    "revision": "cb5f0156c36213a985f2b5acd8f6ac57",
+    "url": "/portfolio/static/media/cloud4.cb5f0156.png"
+  },
+  {
+    "revision": "9dc5b8a8ac805d637a5dce90945fe6c3",
+    "url": "/portfolio/static/media/dindle1.9dc5b8a8.png"
+  },
+  {
+    "revision": "4c2703e6b9e241681b9525f1d7f9733e",
+    "url": "/portfolio/static/media/dindle4.4c2703e6.png"
+  },
+  {
+    "revision": "84fc497f40ac7932a3275cd285a6afb4",
+    "url": "/portfolio/static/media/firebase.84fc497f.png"
+  },
+  {
+    "revision": "43f0d00b776b1f58eac8404e04e8c260",
+    "url": "/portfolio/static/media/gmail.43f0d00b.png"
+  },
+  {
+    "revision": "677a423ecfc7eb0ece64d16c17321f96",
+    "url": "/portfolio/static/media/grassland.677a423e.png"
+  },
+  {
+    "revision": "baa2d2046522a32abf5b76ac1f39fabb",
+    "url": "/portfolio/static/media/help.baa2d204.png"
+  },
+  {
+    "revision": "03f40b67b63d9c1c1a5a792109bbc699",
+    "url": "/portfolio/static/media/instagram.03f40b67.png"
+  },
+  {
+    "revision": "74c0abedb4b48df464dd730e22799d2b",
+    "url": "/portfolio/static/media/laney.74c0abed.jpg"
+  },
+  {
+    "revision": "ffacb2c0bf8c3cf3a560de26e2518509",
+    "url": "/portfolio/static/media/notapp1.ffacb2c0.PNG"
+  },
+  {
+    "revision": "ed6c2492817aec13c1aff7932134193f",
+    "url": "/portfolio/static/media/notapp2.ed6c2492.PNG"
+  },
+  {
+    "revision": "212e8ef373f3ca426d04548140840a15",
+    "url": "/portfolio/static/media/oaklandHigh.212e8ef3.jpg"
+  },
+  {
+    "revision": "8c383c7c063a8d2078ac13b01c80e4ee",
+    "url": "/portfolio/static/media/powerbutton.8c383c7c.jpg"
+  },
+  {
+    "revision": "01b3cdfa28ef19e8521b944c3b7080e2",
+    "url": "/portfolio/static/media/profile.01b3cdfa.jpg"
+  },
+  {
+    "revision": "fd6984368ba75ec90b8f9fe0cae05566",
+    "url": "/portfolio/static/media/python.fd698436.png"
+  },
+  {
+    "revision": "d14e4fd3c8b856f05612643d6773e497",
+    "url": "/portfolio/static/media/react.d14e4fd3.png"
+  },
+  {
+    "revision": "880aaac8d5922b79a1fdbc472a73e8e6",
+    "url": "/portfolio/static/media/restart.880aaac8.png"
+  },
+  {
+    "revision": "71697e2495803d61868ed38f7507e609",
+    "url": "/portfolio/static/media/resume.71697e24.png"
+  },
+  {
+    "revision": "855356d4fce3616fdb807e659ec79061",
+    "url": "/portfolio/static/media/shutdown.855356d4.png"
+  },
+  {
+    "revision": "9179049f191f223af12b5592f6241766",
+    "url": "/portfolio/static/media/tabIcon.9179049f.png"
+  },
+  {
+    "revision": "fd7a8d696422bd8f958f7bfee52e13f4",
+    "url": "/portfolio/static/media/td.fd7a8d69.png"
+  },
+  {
+    "revision": "daba767fe4c236d2f86ede81b29d59b7",
+    "url": "/portfolio/static/media/uci.daba767f.png"
+  },
+  {
+    "revision": "7b40736a6ecd39a840233dc23acb9de6",
+    "url": "/portfolio/static/media/windowslogo.7b40736a.png"
+  }
+]);
